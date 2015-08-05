f95db02dee6e4d14410c3f218d18480fa2e4dff7
Aurelia Pasat <aureliapasat@MHUT197.local>
2015-08-05 13:58:31 +0300

    Changed board object. Created random colors on tiles.

--- Assets/Scripts/Selectable.cs
+++ Assets/Scripts/Selectable.cs
@@ -1,22 +1,33 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
-public enum TileType {Red, Green, Blue, White, Pink, Purple, COUNT};
+//public enum TileType {
+//	Red = 0xff0a0a, 
+//	Green = 0x0ecd17, 
+//	Blue = 0x007eff, 
+//	White = 0xffffff, 
+//	Pink = 0xf481d5, 
+//	Purple = 0x7a08a5};
 
 public class Selectable : MHBaseClass
 {
-	public TileType type;
+	public int type;
 
 	bool isSelected = false;
+
+	string[] tileColors = new string[]{"ff0a0a", "47ad72", "007eff", "ffffff", "f481d5", "7a08a5"};
 
 	GameObject selectionObject = null;
 
 	void Start ()
 	{
 		selectionObject = gameObject.transform.FindChild ("Selection").gameObject;
-		type = (TileType) Random.Range(0, (int)TileType.COUNT);
+		type = Random.Range(0, tileColors.Length);
 
+		Image bg = gameObject.GetComponent<Image> ();
+		bg.color = HexToColor(tileColors[type]);//tileColors[type];
 		eventBus.AddListener<PointerEvent.OnSelectionChanged> (OnSelectionChanged);
 	}
 
@@ -26,12 +37,20 @@ public class Selectable : MHBaseClass
 			isSelected = eventData.selected;
 			selectionObject.SetActive (isSelected);
 
-			Debug.Log(type);
 		}
 	}
 
 	void OnDestroy ()
 	{
 		eventBus.RemoveListener<PointerEvent.OnSelectionChanged> (OnSelectionChanged);
+	}
+
+
+	Color HexToColor(string hex)
+	{
+		byte r = byte.Parse(hex.Substring(0,2), System.Globalization.NumberStyles.HexNumber);
+		byte g = byte.Parse(hex.Substring(2,2), System.Globalization.NumberStyles.HexNumber);
+		byte b = byte.Parse(hex.Substring(4,2), System.Globalization.NumberStyles.HexNumber);
+		return new Color(r/255.0f,g/255.0f,b/255.0f, 255/255.0f);
 	}
 }
