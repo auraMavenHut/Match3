3550f7c607533ea904cbe870ecc50d6e4f795bee
Cristina Velcea <cristina.velcea@mavenhut.com>
2015-08-06 14:17:18 +0300

    Properly counted number of selected items.

--- Assets/Scripts/Managers/DragManager.cs
+++ Assets/Scripts/Managers/DragManager.cs
@@ -2,9 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
 
-public class DragManager : MHBaseClass, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class DragManager : MHBaseClass, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
 	LinkedList<GameObject> selectedObjects = new LinkedList<GameObject> ();
+
+	void Start ()
+	{
+		eventBus.AddListener<PointerEvent.OnSelected> (OnSelected);
+	}
+
+	void OnSelected (PointerEvent.OnSelected eventData)
+	{
+		selectedObjects.AddLast (eventData.targetObject);
+	}
+
+	void OnDestroy ()
+	{
+		eventBus.RemoveListener<PointerEvent.OnSelected> (OnSelected);
+	}
+
+	public void OnPointerDown (PointerEventData eventData)
+	{
+		RaycastHit2D hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (Input.mousePosition), Vector2.zero);
+		if (hit.collider != null) {
+			eventBus.Publish (new PointerEvent.OnSelectionChanged (hit.transform.gameObject, true));
+		}
+	}
 
 	public void OnBeginDrag (PointerEventData eventData)
 	{
@@ -16,8 +39,6 @@ public class DragManager : MHBaseClass, IBeginDragHandler, IDragHandler, IEndDra
 		RaycastHit2D hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (Input.mousePosition), Vector2.zero);
 		if (hit.collider != null) {
 			eventBus.Publish (new PointerEvent.OnSelectionChanged (hit.transform.gameObject, true));
-
-			selectedObjects.AddLast (hit.transform.gameObject);
 		}
 	}
 
--- Assets/Scripts/Selectable.cs
+++ Assets/Scripts/Selectable.cs
@@ -4,11 +4,11 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 
 //public enum TileType {
-//	Red = 0xff0a0a, 
-//	Green = 0x0ecd17, 
-//	Blue = 0x007eff, 
-//	White = 0xffffff, 
-//	Pink = 0xf481d5, 
+//	Red = 0xff0a0a,
+//	Green = 0x0ecd17,
+//	Blue = 0x007eff,
+//	White = 0xffffff,
+//	Pink = 0xf481d5,
 //	Purple = 0x7a08a5};
 
 public class Selectable : MHBaseClass
@@ -17,33 +17,36 @@ public class Selectable : MHBaseClass
 	public int column, row;
 	bool isSelected = false;
 
-	string[] tileColors = new string[]{"bc5879", "32a994", "e8bfa5", "80baf0", "b18dd3"};
+	string[] tileColors = new string[]{ "bc5879", "32a994", "e8bfa5", "80baf0", "b18dd3" };
 
 	GameObject selectionObject = null;
 
-	void Awake()
+	void Awake ()
 	{
-		type = Random.Range(0, tileColors.Length);
+		type = Random.Range (0, tileColors.Length);
 	}
+
 	void Start ()
 	{
 		selectionObject = gameObject.transform.FindChild ("Selection").gameObject;
+
 		Image bg = gameObject.GetComponent<Image> ();
-		bg.color = HexToColor(tileColors[type]);//tileColors[type];
+		bg.color = HexToColor (tileColors [type]);
+
 		eventBus.AddListener<PointerEvent.OnSelectionChanged> (OnSelectionChanged);
 	}
 
 	void OnSelectionChanged (PointerEvent.OnSelectionChanged eventData)
 	{
-		if (eventData.targetObject == gameObject) {
-			isSelected = eventData.selected;
+		if (eventData.targetObject == gameObject && isSelected != eventData.isSelected) {
+			isSelected = eventData.isSelected;
 			selectionObject.SetActive (isSelected);
 
-			if(isSelected)
-			{
-				Debug.Log("c = " + column + "r = " + row);
-			}
+			if (isSelected) {
+				//Debug.Log("c = " + column + "r = " + row);
 
+				eventBus.Publish (new PointerEvent.OnSelected (eventData.targetObject));
+			}
 		}
 	}
 
@@ -53,11 +56,11 @@ public class Selectable : MHBaseClass
 	}
 
 
-	Color HexToColor(string hex)
+	Color HexToColor (string hex)
 	{
-		byte r = byte.Parse(hex.Substring(0,2), System.Globalization.NumberStyles.HexNumber);
-		byte g = byte.Parse(hex.Substring(2,2), System.Globalization.NumberStyles.HexNumber);
-		byte b = byte.Parse(hex.Substring(4,2), System.Globalization.NumberStyles.HexNumber);
-		return new Color(r/255.0f,g/255.0f,b/255.0f, 255/255.0f);
+		byte r = byte.Parse (hex.Substring (0, 2), System.Globalization.NumberStyles.HexNumber);
+		byte g = byte.Parse (hex.Substring (2, 2), System.Globalization.NumberStyles.HexNumber);
+		byte b = byte.Parse (hex.Substring (4, 2), System.Globalization.NumberStyles.HexNumber);
+		return new Color (r / 255.0f, g / 255.0f, b / 255.0f, 255 / 255.0f);
 	}
 }
