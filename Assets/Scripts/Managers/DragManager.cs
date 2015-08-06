2ca4d9686d3e7333d3ded6eddaa26fa1614c98fb
Cristina Velcea <cristina.velcea@mavenhut.com>
2015-08-06 16:15:12 +0300

    Fixed deselection on clicked items.

--- Assets/Scripts/Managers/DragManager.cs
+++ Assets/Scripts/Managers/DragManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
 
-public class DragManager : MHBaseClass, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class DragManager : MHBaseClass, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
 	LinkedList<GameObject> selectedObjects = new LinkedList<GameObject> ();
 
@@ -21,7 +21,7 @@ public class DragManager : MHBaseClass, IPointerDownHandler, IBeginDragHandler, 
 		eventBus.RemoveListener<PointerEvent.OnSelected> (OnSelected);
 	}
 
-	public void OnPointerDown (PointerEventData eventData)
+	void NotifySelectHitObject(PointerEventData eventData)
 	{
 		RaycastHit2D hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (Input.mousePosition), Vector2.zero);
 		if (hit.collider != null) {
@@ -29,25 +29,27 @@ public class DragManager : MHBaseClass, IPointerDownHandler, IBeginDragHandler, 
 		}
 	}
 
-	public void OnBeginDrag (PointerEventData eventData)
-	{
-		
-	}
-
-	public void OnDrag (PointerEventData eventData)
-	{
-		RaycastHit2D hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (Input.mousePosition), Vector2.zero);
-		if (hit.collider != null) {
-			eventBus.Publish (new PointerEvent.OnSelectionChanged (hit.transform.gameObject, true));
-		}
-	}
-
-	public void OnEndDrag (PointerEventData eventData)
+	void NotifyDeselectHitObjects()
 	{
 		foreach (GameObject selectedObject in selectedObjects) {
 			eventBus.Publish (new PointerEvent.OnSelectionChanged (selectedObject, false));
 		}
 
 		selectedObjects.Clear ();
+	}
+
+	public void OnPointerDown (PointerEventData eventData)
+	{
+		NotifySelectHitObject (eventData);
+	}
+
+	public void OnPointerUp (PointerEventData eventData)
+	{
+		NotifyDeselectHitObjects ();
+	}
+
+	public void OnDrag (PointerEventData eventData)
+	{
+		NotifySelectHitObject (eventData);
 	}
 }
